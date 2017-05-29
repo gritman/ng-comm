e6b8fb89dfdeb91781a58ec0668df8e9eac0d14c
gritman <gritman@126.com>
2017-05-29 19:25:05 +0800

    输入属性示例,父组件传属性给子组件,但不能反过来

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-order',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order.component.css']
 })
 export class OrderComponent implements OnInit {
+
+  @Input()
+  stockCode: string;
+
+  @Input()
+  amount: number;
 
   constructor() { }
 
